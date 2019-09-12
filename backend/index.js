50df027a79f771ec64ebbc06069b70cbf5772586
Rami <rami.albadri77@gmail.com>
2019-09-12 16:32:27 +0200

    create and connecte to db

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const RESTserver = require('the.rest');
+ 
+// Connect to MongoDB via Mongoose
+mongoose.connect('mongodb://localhost/time-for-friends', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const db = mongoose.connection;
+ 
+// Create an Express server
+const app = express();
+ 
+// ..and install the.rest as middleware
+// you need to tell it two things:
+// 1) the base route to its api
+// 2) the path to a folder with mongoose-models 
+//    Please Note: The path must be absolute
+const pathToModelFolder = path.join(__dirname, 'mongoose-models');
+app.use(RESTserver('/api', pathToModelFolder));
+ 
+// Add other middleware you might need (express.static etc)
+ 
+// Listen on port 5000
+app.listen(5000, () => console.log('Listening on port 5000'));
