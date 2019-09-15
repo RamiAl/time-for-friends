d71fadebd950e8301815d0297af74dc49b2df603
Rami <rami.albadri77@gmail.com>
2019-09-15 16:37:57 +0200

    get input ((friend info) )from the user and save it in the DB

--- src/AddFriend.js
+++ src/AddFriend.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
+import {Friend} from 'the.rest/dist/to-import';
 
 export default class AddFriend extends Component {
 
@@ -21,57 +22,87 @@ export default class AddFriend extends Component {
         //console.log(mom);
     
     }
-    gotEmail(event){
-        event.preventDefault();
-        console.log(event.target);
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            phoneNumber: '',
+            emailAddress: '',
+            city: '',
+            country: '',
+            timeZone: ''
+          };
+        this.handleUserInput = this.handleUserInput.bind(this);
+    } 
+
+    handleUserInput (e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({[name]: value});
+      }
+
+    async onSubmit(e){
+        e.preventDefault();
+        let friend = new Friend (this.state);
+        await friend.save();
+        
+        let allFriends = await Friend.find().catch(console.error());
+        console.log('All friends', allFriends);
+        //allFriends.delete();
+        //console.log('All deleted', allFriends);
+
+        
     }
-    
-    
 
     render() {
         return (
-            <Form style = {{margin: 50}} onClick={this.gotEmail}>
+            <Form style = {{margin: 50}} onSubmit={(e) => this.onSubmit(e)}>
             <Form.Row>
-                <Form.Group as={Col} controlId="formGridName" >
+                <Form.Group as={Col} controlId="formGridName" value={this.state.name} 
+                onChange={this.handleUserInput} >
                 <Form.Label >First name</Form.Label>
-                <Form.Control type="name" placeholder="Enter name" />
+                <Form.Control name="firstName" placeholder="Enter name" />
                 </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Group as={Col} controlId="formGridPassword" value={this.state.lastName} 
+                onChange={this.handleUserInput}>
                 <Form.Label>Last name</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control name="lastName" placeholder="Last name" />
                 </Form.Group>
             </Form.Row>
 
             <Form.Row>
-            <Form.Group as={Col} controlId="formGridAddress1">
+            <Form.Group as={Col} controlId="formGridAddress1" value={this.state.phoneNumber} 
+                onChange={this.handleUserInput}>
                 <Form.Label>Phone number</Form.Label>
-                <Form.Control placeholder="Phone number" />
+                <Form.Control name="phoneNumber" placeholder="Phone number" />
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Group as={Col} controlId="formGridEmail" value={this.state.emailAddress} 
+                onChange={this.handleUserInput}>
                 <Form.Label>Email address</Form.Label>
-                <Form.Control placeholder="Phone number" />
+                <Form.Control name="emailAddress" placeholder="Email address" />
             </Form.Group>
             </Form.Row>
 
             <Form.Row>
-                <Form.Group as={Col} controlId="formGridCity">
+                <Form.Group as={Col} controlId="formGridCity" value={this.state.city} 
+                onChange={this.handleUserInput}>
                 <Form.Label>City</Form.Label>
-                <Form.Control />
+                <Form.Control name="city" placeholder="City"/>
                 </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridZip">
+                <Form.Group as={Col} controlId="formGridZip" value={this.state.country} 
+                onChange={this.handleUserInput}>
                 <Form.Label>Country</Form.Label>
-                <Form.Control />
+                <Form.Control name="country" placeholder="Country"/>
                 </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridState">
+                <Form.Group as={Col} controlId="formGridState" value={this.state.timeZone} 
+                onChange={this.handleUserInput}>
                 <Form.Label>Time zone</Form.Label>
-                <Form.Control as="select">
-                    <option>Choose...</option>
-                    <option>...</option>
-                </Form.Control>
+                <Form.Control name="timeZone" placeholder="Capital"/>
                 </Form.Group>
 
             </Form.Row>
