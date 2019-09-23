55e16d2ec167aa044d4295447ad5844bfcf2c4d2
Rami <rami.albadri77@gmail.com>
2019-09-23 14:15:59 +0200

    the right email regEx

--- src/AddFriend.js
+++ src/AddFriend.js
@@ -40,8 +40,9 @@ export default class AddFriend extends Component {
     }
 
     validateEmail(email) {   
-        const nameRegex = new RegExp("^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$");
-        return nameRegex.test(email);
+        const emailRegex = new RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
+
+        return emailRegex.test(email);
     }
 
     render() {
