30dc64b43e78b1626ac1e7b9aa857e674e2eee1c
Unknown <filip98@spray.se>
2019-09-26 14:20:43 +0200

    Bacic help on timezon done only on City

--- src/AddFriend.js
+++ src/AddFriend.js
@@ -6,13 +6,10 @@ import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import {Friend} from 'the.rest/dist/to-import';
 import moment from 'moment-timezone';
-//import { all } from 'q';
-//import { arrowFunctionExpression } from '@babel/types';
 const allTimeZone =  moment.tz.names();
 export default class AddFriend extends Component {
 
     async componentDidMount(){
-    
     }
     constructor(props) {
         super(props);
@@ -26,20 +23,13 @@ export default class AddFriend extends Component {
             timeZone: ''
           };
         this.handleUserInput = this.handleUserInput.bind(this);
-       
     } 
 
     async handleUserInput (e) {
         const name = e.target.name;
         const value = e.target.value;
        await this.setState({[name]: value});
-        //this.check();
-        console.log(this.state.timeZone);
-        
-        
     }           
-
-
 
 check(){
     console.log(this.state.city);
@@ -49,45 +39,28 @@ check(){
         })
       }
     filterItems(allTimeZone, this.state.city).map(x => console.log(x));
-
-
-
 }
 
 
-showlist(){
-    /*ska börga mend land kan man bortse
-    sedan stad om den fins 
-    sist inget*/
-    console.log(this.state.country);
-    console.log(this.timeZone);
-
-if (this.state.city  !== '') {
-    console.log('1');
- 
-    if(allTimeZone.filter( item => item.toLocaleLowerCase().indexOf(this.state.city.toLowerCase()) !== -1).map(item =>(<option key = {item}>{item}</option>)).length > 0){
+showTimeZoneList(){
+let c = allTimeZone.filter( item => this.state.city !== "" && item.toLowerCase().includes(this.state.city.toLowerCase()));
+if(c.length > 0){
+    if(c.map(item =>(<option key = {item}>{item}</option>).length > 0)){
         setTimeout(()=>{
             const v = this.refs.timeZone.value;
             if (this.state.timeZone !== v) {
                 this.setState({timeZone: v})
             }
-        }, 0);
-        return allTimeZone.filter( item => item.toLocaleLowerCase().indexOf(this.state.city.toLowerCase()) !== -1)
-        .map(item =>( <option key = {item}>{item}</option>));;
+        }, 0);        
+        return c.map(item =>(<option key = {item}>{item}</option>));
     }
-
+}else{
     return allTimeZone.map(item =>(
         <option key = {item}>{item}</option>
     ));
-}else {
-   return allTimeZone.map(item =>(
-        <option key = {item}>{item}</option>
-    ));
 }
-
-
+   
 }
-
     async onSubmit(e){
         e.preventDefault();
         console.log(this.state);
@@ -101,40 +74,19 @@ if (this.state.city  !== '') {
         this.refs.form.reset();
     }
 
-    /*
-    <button onClick={(e) => this.myFunction(e)}>Click me</button>
-     //this.myFunction = this.myFunction.bind(this);
-     myFunction(e) {
-        console.log('Ggg');
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(function(positon){
-                console.log(positon);
-                
-            });
-            
-        }else{
-              console.log("no geolocation position");  
-            }
-      }
- */
-
-/* {allTimeZone.map(item =>(
-                        <option key = {item}>{item}</option>
-                    ))} */
-
     render() {
         console.log(this.state.city);
         return (
             <>
             <Form style = {{margin: 50}} onSubmit={(e) => this.onSubmit(e)} ref="form">
             <Form.Row>
-                <Form.Group as={Col} controlId="formGridName" value={this.state.name} 
+                <Form.Group as={Col} controlId="formGridFirstName" value={this.state.name} 
                 onChange={this.handleUserInput} >
                 <Form.Label >First name</Form.Label>
                 <Form.Control  name="firstName" placeholder="Enter name" />
                 </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridPassword" value={this.state.lastName} 
+                <Form.Group as={Col} controlId="formGridLastName" value={this.state.lastName} 
                 onChange={this.handleUserInput}>
                 <Form.Label>Last name</Form.Label>
                 <Form.Control name="lastName" placeholder="Last name" />
@@ -142,7 +94,7 @@ if (this.state.city  !== '') {
             </Form.Row>
 
             <Form.Row>
-            <Form.Group as={Col} controlId="formGridAddress1" value={this.state.phoneNumber} 
+            <Form.Group as={Col} controlId="formGridPhoneNumber" value={this.state.phoneNumber} 
                 onChange={this.handleUserInput}>
                 <Form.Label>Phone number</Form.Label>
                 <Form.Control name="phoneNumber" placeholder="Phone number" />
@@ -159,23 +111,20 @@ if (this.state.city  !== '') {
                 <Form.Group as={Col} controlId="formGridCity" value={this.state.city} 
                 onChange={this.handleUserInput}>
                 <Form.Label>City</Form.Label>
-                <Form.Control name="city" placeholder="City"/>
+                <Form.Control name="city" placeholder="City" ref ="city"/>
                 </Form.Group>
 
-                <Form.Group as={Col} controlId="formGridZip" value={this.state.country} 
+                <Form.Group as={Col} controlId="formGridCountry" value={this.state.country} 
                 onChange={this.handleUserInput}>
                 <Form.Label>Country</Form.Label>
-                <Form.Control name="country" placeholder="Country"/>
+                <Form.Control name="country" placeholder="Country" ref ="country"/>
                 </Form.Group>
 
-                
-
-                <Form.Group as={Col} controlId="formGridState" value={this.state.timeZone} 
+                <Form.Group as={Col} controlId="formGridTimeZone" value={this.state.timeZone} 
                 onChange={this.handleUserInput} >
                 <Form.Label>Time zone</Form.Label>
                 <Form.Control as="select" name="timeZone" ref ="timeZone">
-                    {this.showlist()}
-                    
+                    {this.showTimeZoneList()}
                 </Form.Control >
                 </Form.Group>
 
